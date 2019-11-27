65301709b354f25c42f195bf518526fa24dde56c
luoqian <szuluoqian@163.com>
2019-11-27 13:54:04 +0800

    fix(pack): 去掉devserver默认open

--- mlz-pack.js
+++ mlz-pack.js
@@ -2,6 +2,9 @@ const path = require('path');
 module.exports = {
   webpack: {
     publicPath: './',
+    devServer:{
+      open:false
+    },
     htmlPlugin: {
       filename: 'index.html',
       template: path.resolve(__dirname, './src/app/index.ejs'),
