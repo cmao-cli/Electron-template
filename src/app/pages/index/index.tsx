c15ab4e709c7a36b294da55daac8aefe766988f1
luoqian <szuluoqian@163.com>
2019-12-26 05:04:04 +0800

    feat(ipc): 添加主进程和渲染进程通信例子

--- src/app/pages/index/index.tsx
+++ src/app/pages/index/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import * as cx from 'classnames';
 import { bindActionCreators } from 'redux';
-
 import { IReduxState } from '../../redux/root-reducer';
 import { IDemoState, addNum, minusNum } from '../../redux/demo';
+import * as cx from 'classnames';
+
+import { remote, ipcRenderer  } from 'electron';
+const { shell } = remote
+
 import './index.scss';
 
 interface IIndexProps {
@@ -13,7 +16,21 @@ interface IIndexProps {
   minusNum:typeof minusNum;
 }
 
+
 class Index extends React.PureComponent<IIndexProps> {
+  openBrowser() {
+    //打开默认浏览器
+    shell.openExternal('http://www.baidu.com');
+    
+    // send方法发送的消息会被electron序列化，方法和原型会被去掉
+    ipcRenderer.send('echo', {
+      name: 'luoqian',
+      age: 23,
+      say: () => {
+        console.log('my name is luoqian');
+      }
+    });
+  }
   private _divider = 2;
   render() {
     const { num } = this.props.demoState;
@@ -24,6 +41,8 @@ class Index extends React.PureComponent<IIndexProps> {
           <span styleName={cx('is_black', num % this._divider === 0 && 'is_red')}>{num}</span>
           <button onClick={() => {this.props.addNum(1); }}>+</button>
           <button onClick={() => {this.props.minusNum(1); }}>-</button>
+          <a target="_blank" href={'http://www.baidu.com'}>打开新窗口</a>
+          <a href="javascript:(void 0)" onClick={() => { this.openBrowser() }}>打开浏览器窗口</a>
         </div>
       </div>
     );
